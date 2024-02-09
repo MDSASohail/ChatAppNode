422a865fdc17dec003771ee04edb8037da335c37
Sohail <thisemailitakenbysohail@gmail.com>
2024-02-09 17:26:31 +0530

    Adding origin

--- App.js
+++ App.js
@@ -20,7 +20,7 @@ app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 dotenv.config();
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Origin', "https://mdsasohail.github.io/");
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
     res.header('Access-Control-Allow-Credentials', true);
