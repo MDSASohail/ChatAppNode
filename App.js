ad5654b93dfc561b7d880d5d178989a9dc62f9da
Sohail <thisemailitakenbysohail@gmail.com>
2024-02-09 17:43:07 +0530

    H

--- App.js
+++ App.js
@@ -19,13 +19,13 @@ const dotenv=require('dotenv');
 app.use(express.urlencoded({extended:true}))
 app.use(express.json())
 dotenv.config();
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', "https://mdsasohail.github.io");
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-  });
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', "https://mdsasohail.github.io");
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+//     res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+//     res.header('Access-Control-Allow-Credentials', true);
+//     next();
+//   });
 async function Connect()
 {
     try {
